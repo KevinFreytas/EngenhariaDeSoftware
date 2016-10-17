7ac51370e3485a3cc295cb536d2e3fb2405a0a41
kevin.nascimento <kevin.nascimento@LAB1-DCX-34.dcx.ufpb.br>
2016-10-17 08:55:51 -0300

    Commitando teste 1

---
+++ src/AulaPratica17_10_16/Zoberi.java
@@ -0,0 +1,17 @@
+package AulaPratica17_10_16;
+
+public class Zoberi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+// Passos para linkar com o GitHub:
+
+// Criar projeto > pacote > classe
+// Botão direito no nome da classe > Team > Share Project > Create repository
+// Botão direito no nome da classe > Team > Add do Index
+// Team > Push Branch 'master'
+// 
