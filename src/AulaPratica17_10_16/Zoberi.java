3f9188081f150d4e0e72c3bb0c6700c71d7f8c19
kevin.nascimento <kevin.nascimento@LAB1-DCX-34.dcx.ufpb.br>
2016-10-17 09:19:39 -0300

    Modificação nos comentários

--- src/AulaPratica17_10_16/Zoberi.java
+++ src/AulaPratica17_10_16/Zoberi.java
@@ -12,8 +12,9 @@ public class Zoberi {
 
 // Passos para linkar com o GitHub:
 
-// Criar projeto > pacote > classe
+// Criar projeto > Pacote > Classe
 // Botão direito no nome da classe > Team > Share Project > Create repository
 // Botão direito no nome da classe > Team > Add do Index
 // Team > Push Branch 'master'
-// 
+// Team > Commit > Commit and Push > Copiar link http > Nome de usuário e senha > Next > Next
+// Após mudança no programa > Team > Commit > Commit and Push
